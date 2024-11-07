df2a9aafdfea09b30a19a92241e1b892a2d80e56
Zohaib Khan <zohaibk1111@gmail.com>
2024-11-07 01:55:11 -0500

    News api with input funciton

--- rest_apis/rest_api_intro.py
+++ rest_apis/rest_api_intro.py
@@ -1,4 +1,32 @@
 import requests
-r = requests.get("https://newsapi.org/v2/everything?q=tesla&from=2024-10-05&sortBy=publishedAt&apiKey=1d1bb7a6be7e45b4a462923c03521274")
-r = r.json()
-print(r)
+
+class MyCustomError(Exception):
+    def __init__(self, message, error_code=None):
+        super().__init__(message)
+        self.error_code = error_code
+
+def get_news(topic, from_date, to_date, language='en', api_key=''):
+    try:
+        r = requests.get(f"https://newsapi.org/v2/everything?qInTitle={topic}&from={from_date}&to={to_date}8&sortBy=popularity&language={language}&apiKey={api_key}")
+        r = r.json()
+        articles = r['articles']
+    except KeyError as e :
+        raise  KeyError("Something went wrong")
+
+
+    for article in articles:
+        print(f"article:  {article}")
+
+
+
+def main():
+    topic  = input("Enter topic: ")
+    date_start = input("Enter Start Date: ")
+    date_end = input("Enter End Date: ")
+    lang = input("Enter Lang: ")
+    api = input("Enter Api Key: ")
+
+    get_news(topic, date_start, date_end,language=lang, api_key=api)
+
+
+main()
