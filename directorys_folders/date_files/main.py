65bc50e780fcdf633094485427828737b8c7c2c6
Zohaib Khan <zohaibk1111@gmail.com>
2025-04-22 23:18:25 -0400

    update

---
+++ directorys_folders/date_files/main.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+from datetime import datetime
+
+
+if __name__ == '__main__':
+    path = Path("files")
+
+    files = path.glob("**/*")
+    for val in files:
+        
+        if val.is_file():
+            # print(val, "VAL")
+            stats = val.stat().st_ctime
+            # print(stats)
+            date_created = datetime.fromtimestamp(stats)
+            date_created_str = date_created.strftime("%Y-%m-%d_%H:%M:%S")
+            print(date_created_str)
+            new_name = date_created_str + "-" + val.name
+            print(new_name)
+            new_path = val.with_name(new_name)
+            print(new_path)
+            val.rename(new_path)
+
+
+    # seconds_created = path.stat().st_ctime
+    # date_created = datetime.fromtimestamp(seconds_created)
+    # date_created_str = date_created.strftime("%Y-%m-$d_%H:%M:%S")
+    # print(f"File created on: {date_created}")
+
+
