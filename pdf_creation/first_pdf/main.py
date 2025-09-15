f21ea3b66cf5d602c56513b552a2098dbeff43e1
Zohaib Khan <zohaibk1111@gmail.com>
2025-09-15 00:50:37 -0400

    adding borders to your pdf

--- pdf_creation/first_pdf/main.py
+++ pdf_creation/first_pdf/main.py
@@ -6,11 +6,18 @@ if __name__ == "__main__":
     pdf = FPDF(orientation='P', unit='pt', format='letter')
     pdf.add_page()
 
-    pdf.image('assets/download-1.jpg',w=80, h=50)
+    pdf.image('assets/download-1.jpg',w=80, h=50, x=500)
+
+    pdf.set_font(family='Times', size=24, style='B')
+    pdf.cell(w=0,h=50, txt="My First PDF", align='C', border=1)
+
+    pdf.set_font(family='Times', size=14, style='B')
+    pdf.cell(w=0,h=50, txt="Description", )
 
     output_dir = Path('pdfs')
     output_dir.mkdir(exist_ok=True)
 
+   
 
     pdf.output(output_dir / 'my_first_pdf.pdf')
 
