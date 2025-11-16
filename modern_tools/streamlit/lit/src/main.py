a1e05d65cdbb9f0ef959782ff578763a65214602
Zohaib Khan <zohaibk1111@gmail.com>
2025-11-16 03:15:17 -0500

    adding a table

--- modern_tools/streamlit/lit/src/main.py
+++ modern_tools/streamlit/lit/src/main.py
@@ -1,7 +1,17 @@
 import streamlit as st
+import pandas as pd
 
+
+data = {
+    'Series_1': [1, 2, 3, 4, 5],
+    'Series_2': [10, 20, 30, 40, 50]
+
+}
+df = pd.DataFrame(data)
 st.title("Our first streamlit App")
 st.subheader("Introducing streamlit in automate everything with python")
 st.write('''This is our first web app.
          Enjoy it
          ''')
+
+st.write(df)
