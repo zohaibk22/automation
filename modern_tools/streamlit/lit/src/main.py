e92d09d44ce712e750865a03bc4326a7aaa79fb2
Zohaib Khan <zohaibk1111@gmail.com>
2025-11-16 02:26:09 -0500

    streamlit

---
+++ modern_tools/streamlit/lit/src/main.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("Our first streamlit App")
+st.subheader("Introducing streamlit in automate everything with python")
+st.write('''This is our first web app.
+         Enjoy it
+         ''')
