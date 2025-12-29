2e3edba47fd2ef8c4b43b18cbd68428e61bbba7a
Zohaib Khan <zohaibk1111@gmail.com>
2025-12-29 01:29:12 -0500

    updated formating

--- web_gui/currency_converter_gui_2/src/main.py
+++ web_gui/currency_converter_gui_2/src/main.py
@@ -1,5 +1,6 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget, QHBoxLayout
 from PyQt6.QtWidgets import QLabel, QPushButton, QLineEdit, QComboBox
+from PyQt6.QtCore import Qt
 
 from utils.currency_exchange_rate import get_currency
 
@@ -15,34 +16,48 @@ def main():
 
     app = QApplication([])
     window = QWidget()
-    window.setWindowTitle("Currency Converter")
+    window.setWindowTitle("Currency Converter")  
 
-    layout = QVBoxLayout()
+    layout = QVBoxLayout() 
+
+    layout_1 = QHBoxLayout()
+    layout.addLayout(layout_1)
+
+    output_label = QLabel("")
+    layout.addWidget(output_label)
+
+    layout_2 = QVBoxLayout()
+    layout_1.addLayout(layout_2)
+
+    layout_3 = QVBoxLayout()
+    layout_1.addLayout(layout_3)
+
+
     label = QLabel("Enter amount (USD): ")
     layout.addWidget(label)
 
     in_combo = QComboBox()
     currencies = ['USD', 'GBP', 'CAD', 'AUD', 'INR']
     in_combo.addItems(currencies)
-    layout.addWidget(in_combo)
+    layout_2.addWidget(in_combo)
 
     target_combo = QComboBox()
     currencies = ['USD', 'GBP', 'CAD', 'AUD', 'INR']
     target_combo.addItems(currencies)
-    layout.addWidget(target_combo)
+    layout_2.addWidget(target_combo)
    
    
    
     text = QLineEdit()
-    layout.addWidget(text)
+    layout_3.addWidget(text)
 
     btn = QPushButton('Converter to EUR')
-    layout.addWidget(btn)
+    layout_3.addWidget(btn, alignment=Qt.AlignmentFlag.AlignBottom)
 
     btn.clicked.connect(show_currency)
     
-    output_label = QLabel("")
-    layout.addWidget(output_label)
+    # output_label = QLabel("")
+    # layout.addWidget(output_label)
 
     window.setLayout(layout)
 
